4f6ec158450ef99f71b22debea0c5b1fd7436ea8
Zablotskyi <zablotskyivv@gmail.com>
2023-12-17 22:22:45 +0200

    Fixed the problem (added calculation for a negative power number)

--- src/src/com/shpp/p2p/cs/vzablotskyi/Assignment3Part3.java
+++ src/src/com/shpp/p2p/cs/vzablotskyi/Assignment3Part3.java
@@ -9,7 +9,7 @@ public class Assignment3Part3 {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.print("enter the number to be raised to a power: ");
-            base = scanner.nextInt();
+            base = scanner.nextDouble();
             System.out.println();
             System.out.print("enter a power for the numbers: ");
             exponent = scanner.nextInt();
@@ -25,8 +25,15 @@ public class Assignment3Part3 {
     private double raiseToPower(double base, int exponent) {
         double result = base;
         if(exponent == 0) {
+            System.out.println(1);
             return 1;
-        } else {
+        } else if (exponent < 0) {
+            for (int i = 1; i < exponent * -1; i++) {
+                result = result * base;
+            }
+            result = 1 / result;
+        }
+        else {
             for (int i = 1; i < exponent; i++) {
                 result = result * base;
             }
