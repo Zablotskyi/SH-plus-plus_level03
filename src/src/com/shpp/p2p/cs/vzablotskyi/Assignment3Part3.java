fe2cc575f87bd8324a3e5b1e792f3bc4974c06d9
Zablotskyi <zablotskyivv@gmail.com>
2023-12-17 21:55:34 +0200

    completed the task Assignment3Part3

--- src/src/com/shpp/p2p/cs/vzablotskyi/Assignment3Part3.java
+++ src/src/com/shpp/p2p/cs/vzablotskyi/Assignment3Part3.java
@@ -1,4 +1,38 @@
 package src.com.shpp.p2p.cs.vzablotskyi;
 
+import java.util.Scanner;
+
 public class Assignment3Part3 {
+    private static double base;
+    private static int exponent;
+
+    public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.print("enter the number to be raised to a power: ");
+            base = scanner.nextInt();
+            System.out.println();
+            System.out.print("enter a power for the numbers: ");
+            exponent = scanner.nextInt();
+            Assignment3Part3 assignment3Part3 = new Assignment3Part3();
+            assignment3Part3.raiseToPower(base, exponent);
+        } catch (Exception e) {
+            System.out.println("you entered an incorrect number, try again");
+        }
+    }
+
+    /*we get two numbers (the pure number that needs to be raised to a power and the power itself), calculate the
+     result, and print it to the console. If the degree is zero, then we print 1.*/
+    private double raiseToPower(double base, int exponent) {
+        double result = base;
+        if(exponent == 0) {
+            return 1;
+        } else {
+            for (int i = 1; i < exponent; i++) {
+                result = result * base;
+            }
+        }
+        System.out.println(result);
+
+        return result;
+    }
 }
